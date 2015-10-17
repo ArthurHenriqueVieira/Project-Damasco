ba8997c5f1206da6e350e44bb3e9c580f2a8a5b2
hideki16 <gabriel_shippo@hotmail.com>
2015-10-16 22:02:00 -0300

    juntei as mudanças de segundo grau e do fundo do mapa

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_font.h>
@@ -11,9 +11,37 @@
 #define LARGURA 1280
 #define ALTURA 720
 
+ALLEGRO_TIMER *timer;
+ALLEGRO_DISPLAY *display = NULL;
+ALLEGRO_BITMAP *image = NULL;
+ALLEGRO_BITMAP *image2 = NULL;
+ALLEGRO_AUDIO_STREAM *musica = NULL;
+ALLEGRO_SAMPLE *sample = NULL;
+ALLEGRO_BITMAP *botao=0, *botao_g = 0;
+ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+ALLEGRO_FONT *font = NULL, *font2 = NULL;
+
 enum FUNC{CONS, PRIM, SEC};
 enum KEYS{UP, DOWN, LEFT, RIGHT, SPACE};
 
+typedef struct
+{
+    float x;
+    float y;
+    float velX;
+    float velY;
+    int dirX;
+    int dirY;
+    int width;
+    int height;
+
+    ALLEGRO_BITMAP *image;
+
+}Background;
+
+Background BG;
+
+bool initialize();
 void coordenadas();
 void segundograu(float a, float b, float c);
 
@@ -27,138 +55,22 @@ struct projetil
 
 struct projetil bala;
 
-int main(int argc, char *argv[]){
+// Background
+void initback(Background *fundo, float x, float y, float velx, int width, int height, int dirX, ALLEGRO_BITMAP *image);
+void updateback(Background *fundo);
+void drawback(Background *fundo);
+
+int main(){
     bool exit = false;
+    bool render = false;
     bool keys[5] = {false,false,false,false,false};
     bool func[3] = {false, false, false};
     int a = 0;
     int b = 0;
     int c = 0;
-    int x,y;
+    int x, y;
 
-    ALLEGRO_TIMER *timer;
-    ALLEGRO_DISPLAY *display = NULL;
-    ALLEGRO_BITMAP *image = NULL;
-    ALLEGRO_AUDIO_STREAM *musica = NULL;
-    ALLEGRO_SAMPLE *sample = NULL;
-    ALLEGRO_BITMAP *botao=0, *botao_g = 0;
-    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-    ALLEGRO_FONT *font = NULL, *font2 = NULL;
-    
-    if(!al_init()){
-        fprintf(stderr, "failed to initialize allegro!\n");
-        return -1;
-    }
-    
-    display = al_create_display(LARGURA, ALTURA);
-    
-    if(!display){
-        fprintf(stderr, "failed to create display!\n");
-        return -1;
-    }
-
-    if (!al_install_audio()){
-        fprintf(stderr, "Falha ao inicializar áudio.\n");
-        return false;
-    }
- 
-    if (!al_init_acodec_addon()){
-        fprintf(stderr, "Falha ao inicializar codecs de áudio.\n");
-        return false;
-    }
- 
-    if (!al_reserve_samples(1)){
-        fprintf(stderr, "Falha ao alocar canais de áudio.\n");
-        return false;
-    }
-
-    if(!al_install_keyboard()){
-      fprintf(stderr, "failed to initialize the keyboard!\n");
-      return -1;
-    }
-    
-    if(!al_install_mouse()){
-      fprintf(stderr, "failed to initialize the mouse!\n");
-      return -1;
-    }
-
-    if(!al_init_image_addon()){
-        fprintf(stderr, "failed to initialize image\n");
-        return -1;
-    }
-    //if(!al_init_font_addon()){
-    //    fprintf(stderr, "falha ao inicializar a fonte\n");
-    //    return -1;
-    //}
-    if (!al_init_ttf_addon()){
-        fprintf(stderr, "Falha ao inicializar add-on allegro_ttf.\n");
-        return -1;
-    }
-    musica = al_load_audio_stream("bits.ogg", 4, 1024);
-    if (!musica)
-    {
-        fprintf(stderr, "Falha ao carregar audio.\n");
-        al_destroy_event_queue(event_queue);
-        al_destroy_display(display);
-        al_destroy_sample(sample);
-        return false;
-    }
-    
-    sample = al_load_sample("palmas.wav");
-    if (!sample)
-    {
-        fprintf(stderr, "Falha ao carregar sample.\n");
-        al_destroy_display(display);
-        return false;
-    }
-    event_queue = al_create_event_queue();
-    
-    if(!event_queue) {
-        fprintf(stderr, "failed to create event_queue!\n");
-        al_destroy_display(display);
-        return -1;
-    }
-    
-    image = al_load_bitmap("ciao.jpg");
-    
-    botao = al_create_bitmap(30,30);
-    if(!botao){
-        fprintf(stderr, "Falha ao iniciar bitmap\n");
-        al_destroy_display(display);
-        return -1;
-    }
-
-    botao_g = al_create_bitmap(200,100);
-    if(!botao_g){
-        fprintf(stderr, "Falha ao iniciar bitmap\n");
-        al_destroy_display(display);
-        return -1;
-    }
-
-    font = al_load_font("04B_30__.ttf", 45, 0);
-    if(!font){
-        al_destroy_display(display);
-        fprintf(stderr, "Erro ao carregar fonte\n");
-        return -1;
-    }
-    font2 = al_load_font("04B_30__.ttf", 25, 0);
-    if(!font2){
-        al_destroy_display(display);
-        fprintf(stderr, "Erro ao carregar fonte\n");
-        return -1;
-    }
-    
-    al_attach_audio_stream_to_mixer(musica, al_get_default_mixer());
-    al_set_audio_stream_playing(musica, true);
-    
-    timer = al_create_timer(1.0/60);
-    
-    al_register_event_source(event_queue, al_get_display_event_source(display));
-    al_register_event_source(event_queue, al_get_keyboard_event_source());
-    al_register_event_source(event_queue, al_get_mouse_event_source());
-    al_register_event_source(event_queue, al_get_timer_event_source(timer));
-   
-    al_start_timer(timer);
+    initialize();
    
     while(!exit){
         ALLEGRO_EVENT ev;
@@ -179,6 +91,7 @@ int main(int argc, char *argv[]){
                     b += 1;
                     break;
                 case ALLEGRO_KEY_SPACE:
+                    al_play_sample(sample, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL); 
                     keys[SPACE] = true;
                     break; 
             }
@@ -255,14 +168,18 @@ int main(int argc, char *argv[]){
                         a = 0;
                         b = 0;
                         c = 0;
+                        x = 0;
+                        y = 0;
                     }
-
             }
-        if(keys[SPACE] && ev.type == ALLEGRO_EVENT_TIMER)
-            al_play_sample(sample, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL); 
+        if(ev.type == ALLEGRO_EVENT_TIMER){
+            updateback(&BG);
+            render = true;
+        }
         
-        if(al_is_event_queue_empty(event_queue)){
-            al_draw_bitmap(image, 0, 0, 0);
+        if(render && al_is_event_queue_empty(event_queue)){
+            drawback(&BG);
+            al_draw_bitmap(image2, 0, 480, 0);
             //    x=a;
             //    y=b;
             //coordenadas(&x,&y);
@@ -288,7 +205,8 @@ int main(int argc, char *argv[]){
             if(func[0]){
                 al_draw_bitmap(botao, 685, 570, 0);
                 al_draw_bitmap(botao, 685, 650, 0);
-                
+
+                al_draw_line(1150, 500 - (b*20), 200, 500 - (b*20),al_map_rgb(131,139,131), 3);
                 al_draw_text(font, al_map_rgb(0,0,0), 525, 600, ALLEGRO_ALIGN_CENTRE, "F(x) = ");
                 al_draw_textf(font, al_map_rgb(0,0,0), 700, 600, ALLEGRO_ALIGN_CENTRE, "%d", b);
                 
@@ -339,6 +257,124 @@ int main(int argc, char *argv[]){
    
     return 0;
 }
+    
+    bool initialize(){
+        if(!al_init()){
+            fprintf(stderr, "failed to initialize allegro!\n");
+            return -1;
+        }
+        
+        display = al_create_display(LARGURA, ALTURA);
+        
+        if(!display){
+            fprintf(stderr, "failed to create display!\n");
+            return -1;
+        }
+
+        if (!al_install_audio()){
+            fprintf(stderr, "Falha ao inicializar áudio.\n");
+            return false;
+        }
+     
+        if (!al_init_acodec_addon()){
+            fprintf(stderr, "Falha ao inicializar codecs de áudio.\n");
+            return false;
+        }
+     
+        if (!al_reserve_samples(1)){
+            fprintf(stderr, "Falha ao alocar canais de áudio.\n");
+            return false;
+        }
+
+        if(!al_install_keyboard()){
+          fprintf(stderr, "failed to initialize the keyboard!\n");
+          return -1;
+        }
+        
+        if(!al_install_mouse()){
+          fprintf(stderr, "failed to initialize the mouse!\n");
+          return -1;
+        }
+
+        if(!al_init_image_addon()){
+            fprintf(stderr, "failed to initialize image\n");
+            return -1;
+        }
+        al_init_font_addon();
+        
+        if (!al_init_ttf_addon()){
+            fprintf(stderr, "Falha ao inicializar add-on allegro_ttf.\n");
+            return -1;
+        }
+        musica = al_load_audio_stream("bits.ogg", 4, 1024);
+        if (!musica)
+        {
+            fprintf(stderr, "Falha ao carregar audio.\n");
+            al_destroy_event_queue(event_queue);
+            al_destroy_display(display);
+            al_destroy_sample(sample);
+            return false;
+        }
+        
+        sample = al_load_sample("palmas.wav");
+        if (!sample)
+        {
+            fprintf(stderr, "Falha ao carregar sample.\n");
+            al_destroy_display(display);
+            return false;
+        }
+        event_queue = al_create_event_queue();
+        
+        if(!event_queue) {
+            fprintf(stderr, "failed to create event_queue!\n");
+            al_destroy_display(display);
+            return -1;
+        }
+        image2 = al_load_bitmap("chas2.jpg");
+        image = al_load_bitmap("chas.jpg");
+        initback(&BG, 0, 0, 0.2, 1280, 720, -1, image);
+        
+        botao = al_create_bitmap(30,30);
+        if(!botao){
+            fprintf(stderr, "Falha ao iniciar bitmap\n");
+            al_destroy_display(display);
+            return -1;
+        }
+
+        botao_g = al_create_bitmap(200,100);
+        if(!botao_g){
+            fprintf(stderr, "Falha ao iniciar bitmap\n");
+            al_destroy_display(display);
+            return -1;
+        }
+
+        font = al_load_font("04B_30__.ttf", 45, 0);
+        if(!font){
+            al_destroy_display(display);
+            fprintf(stderr, "Erro ao carregar fonte\n");
+            return -1;
+        }
+        font2 = al_load_font("04B_30__.ttf", 25, 0);
+        if(!font2){
+            al_destroy_display(display);
+            fprintf(stderr, "Erro ao carregar fonte\n");
+            return -1;
+        }
+        
+        al_attach_audio_stream_to_mixer(musica, al_get_default_mixer());
+        al_set_audio_stream_playing(musica, true);
+        
+        timer = al_create_timer(1.0/60);
+        
+        al_register_event_source(event_queue, al_get_display_event_source(display));
+        al_register_event_source(event_queue, al_get_keyboard_event_source());
+        al_register_event_source(event_queue, al_get_mouse_event_source());
+        al_register_event_source(event_queue, al_get_timer_event_source(timer));
+       
+        al_start_timer(timer);
+        
+        return true;
+    }
 
 void coordenadas(float *x, float *y){
     float OrigemX = 250;
@@ -366,4 +402,28 @@ void segundograu(float a, float b, float c){
     }
 
     return;
+}
+
+void initback(Background *fundo, float x, float y, float velx, int width, int height, int dirX, ALLEGRO_BITMAP *image){
+    fundo->x = x;
+    fundo->y = y;
+    fundo->velX = velx;
+    fundo->width = width;
+    fundo->height = height;
+    fundo->dirX = dirX;
+    fundo->image = image;
+}
+
+void updateback(Background *fundo){
+    fundo->x += fundo->velX * fundo->dirX;
+    if(fundo->x + fundo->width <= 0)
+        fundo->x = 0;
+}
+
+void drawback(Background *fundo){
+    al_draw_bitmap(fundo->image, fundo->x, fundo->y, 0);
+
+    if((fundo->x + fundo->width) < LARGURA)
+        al_draw_bitmap(fundo->image, (fundo->x + fundo->width), fundo->y, 0);
+
 }
